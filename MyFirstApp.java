f88d39f6823b9561bb48452f717573f27c513126
ashanerlige <ashanh.ubdt@gmail.com>
2019-05-03 17:02:25 +0530

    My first commit...

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -3,5 +3,6 @@ class MyFirstApp
     public static void main(String args[]) 
     { 
         System.out.println("Hello >>>>>>>>>>, World"); 
+        System.out.println("Hello >>>>>>>>>>, World, with first commit........."); 
     }  
 }
