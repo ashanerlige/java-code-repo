302fe505dd71ab6bbb9d44c92982c0e8a856c65a
ashanerlige <50104944+ashanerlige@users.noreply.github.com>
2019-07-11 17:39:18 +0530

    Update MyFirstApp.java
    Commit to check auto build trigger in jenkins

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -4,5 +4,6 @@ class MyFirstApp
     { 
         System.out.println("Hello >>>>>>>>>>, World"); 
         System.out.println("Hello >>>>>>>>>>, World, with first commit........."); 
+        System.out.println("Hello >>>>>>>>>>, Check auto build trigger in jenkins........."); 
     }  
 }
